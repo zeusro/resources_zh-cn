19da9ef65c5ec5016b52eca1b5342363dba8f9bc
zeusro <692486802@QQ.COM>
2015-03-26 10:12:33 +0800

    做winform工具，剩下行比对搜索，写入

--- MyTextAnalyzer/MyTextAnalyzer/Form1.Designer.cs
+++ MyTextAnalyzer/MyTextAnalyzer/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PathOld = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathNew = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PathTarget = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PathOld
+            // 
+            this.PathOld.Location = new System.Drawing.Point(96, 12);
+            this.PathOld.Name = "PathOld";
+            this.PathOld.Size = new System.Drawing.Size(471, 21);
+            this.PathOld.TabIndex = 0;
+            this.PathOld.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\resources_zh-cn-old\\messages";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "旧的汉化目录";
+            // 
+            // PathNew
+            // 
+            this.PathNew.Location = new System.Drawing.Point(96, 39);
+            this.PathNew.Name = "PathNew";
+            this.PathNew.Size = new System.Drawing.Size(471, 21);
+            this.PathNew.TabIndex = 0;
+            this.PathNew.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\resources_zh-cn\\messages";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "目标汉化目录";
+            // 
+            // PathTarget
+            // 
+            this.PathTarget.Location = new System.Drawing.Point(96, 66);
+            this.PathTarget.Name = "PathTarget";
+            this.PathTarget.Size = new System.Drawing.Size(471, 21);
+            this.PathTarget.TabIndex = 0;
+            this.PathTarget.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\MyTextAnalyzer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "输出目录";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result.Location = new System.Drawing.Point(13, 104);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Result.Size = new System.Drawing.Size(649, 395);
+            this.Result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 511);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PathTarget);
+            this.Controls.Add(this.PathNew);
+            this.Controls.Add(this.PathOld);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox PathOld;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PathNew;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PathTarget;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Result;
     }
 }
 
--- MyTextAnalyzer/MyTextAnalyzer/Form1.cs
+++ MyTextAnalyzer/MyTextAnalyzer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,73 @@ namespace MyTextAnalyzer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string Pathold { get; private set; }
+        public string Pathnew { get; private set; }
+        public string Pathtarget { get; private set; }
+
+
+        /// <summary>
+        /// 开始分析
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Action action = new Action(() =>
+            {
+                Clear();
+                Result.AppendText(string.Format("开始装逼{0}", Environment.NewLine));
+                Initialize();
+                var files = Directory.GetFiles(Pathnew, "*.properties", SearchOption.TopDirectoryOnly).ToList();
+                //for (int i = 0; i < files.Count; i++)
+                for (int i = 0; i < 1; i++)
+                {
+                    string propertiePath = files.ElementAt(i);//propertie全路径
+                    Result.AppendText(string.Format("开始分析文件：{1}{0}", Environment.NewLine, propertiePath));
+                    StringWriter sw = new StringWriter();
+                    StringBuilder sb = new StringBuilder();
+
+                    using (StreamReader sr = new StreamReader(propertiePath, Encoding.ASCII))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string a = sr.ReadLine();
+                            //Result.AppendText(string.Format("该行为：{1}{0}", Environment.NewLine, a));
+                        }
+                    }
+
+                }
+                //打开新文件，然后遍历旧文件的每一行，然后替换，追加到sb,最后把sb输出到新目录里面
+                //编码？
+                Result.AppendText(string.Format("装逼结束{0}", Environment.NewLine));
+            });
+            TryCatch(action);
+        }
+
+        private void Initialize()
+        {
+            Pathold = PathOld.Text;
+            Pathnew = PathNew.Text;
+            Pathtarget = PathTarget.Text;
+        }
+
+        private void TryCatch(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Clear()
+        {
+            Result.Clear();
         }
     }
 }
