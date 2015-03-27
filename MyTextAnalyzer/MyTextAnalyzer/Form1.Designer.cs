a27e705391ef46e613098683b3e5c40e159ba68c
zeusro <692486802@QQ.COM>
2015-03-27 10:04:00 +0800

    重新分析

--- MyTextAnalyzer/MyTextAnalyzer/Form1.Designer.cs
+++ MyTextAnalyzer/MyTextAnalyzer/Form1.Designer.cs
@@ -36,6 +36,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.Result = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // PathOld
@@ -104,28 +117,154 @@
             this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.Result.Location = new System.Drawing.Point(13, 104);
+            this.Result.Location = new System.Drawing.Point(13, 240);
             this.Result.Multiline = true;
             this.Result.Name = "Result";
             this.Result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Result.Size = new System.Drawing.Size(649, 395);
+            this.Result.Size = new System.Drawing.Size(652, 372);
             this.Result.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(587, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "切换中文";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(587, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "切换英文";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "中文包目录";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(471, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\backup\\原版";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "英文包目录";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(471, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "C:\\Program Files\\Android\\Android Studio\\lib";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "软件目录";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(471, 21);
+            this.textBox4.TabIndex = 0;
+            this.textBox4.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\MyTextAnalyzer\\Todo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "待汉化目录";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(96, 201);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(471, 21);
+            this.textBox5.TabIndex = 0;
+            this.textBox5.Text = "F:\\MyOpenSourceLife\\resources_zh-cn\\MyTextAnalyzer\\原文";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 207);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 12);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "原文整理目录";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "排序原文";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 511);
+            this.ClientSize = new System.Drawing.Size(682, 624);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.Result);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.PathTarget);
             this.Controls.Add(this.PathNew);
             this.Controls.Add(this.PathOld);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,6 +280,19 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- MyTextAnalyzer/MyTextAnalyzer/Form1.cs
+++ MyTextAnalyzer/MyTextAnalyzer/Form1.cs
@@ -50,6 +50,7 @@ namespace MyTextAnalyzer
                     Result.AppendText(string.Format("开始分析文件：{1}{0}", Environment.NewLine, propertiePath));
                     StringBuilder sb = new StringBuilder();//记录汉化文
                     StringBuilder sb2 = new StringBuilder();//记录原文
+                    List<string> old = new List<string>();
                     using (StreamReader sr = new StreamReader(propertiePath, Encoding.ASCII))
                     {
                         int line = 0;
@@ -61,6 +62,7 @@ namespace MyTextAnalyzer
                                 line++;
                                 continue;
                             }
+                            old.Add(a);
                             //Result.AppendText(string.Format("{2}行为：{1}{0}", Environment.NewLine, a, line++));
                             using (StreamReader sr2 = new StreamReader(Path.Combine(Pathold, fileName), Encoding.ASCII))
                             //读取已汉化文件
@@ -71,6 +73,7 @@ namespace MyTextAnalyzer
                                 {
                                     oldTextList.Add(sr2.ReadLine());
                                 }
+                                //oldTextList = oldTextList.OrderBy(o => o.ToString()).ToList();
                                 string newhead = a.Split('=').First();//原文头部
                                 string fine = oldTextList.Where(o => o.StartsWith(newhead)).FirstOrDefault();//汉化文
                                 if (fine != null)//有汉化文
@@ -90,6 +93,12 @@ namespace MyTextAnalyzer
                         fs.Write(bytes, 0, bytes.Length);
                     }
                     using (FileStream fs = new FileStream(Path.Combine(Pathtarget, fileName), FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                    {
+                        var bytes = System.Text.Encoding.ASCII.GetBytes(sb2.ToString());
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                   
+                    using (FileStream fs = new FileStream(Path.Combine(textBox4.Text, fileName), FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
                     {
                         var bytes = System.Text.Encoding.ASCII.GetBytes(sb2.ToString());
                         fs.Write(bytes, 0, bytes.Length);
@@ -126,6 +135,108 @@ namespace MyTextAnalyzer
         private void Clear()
         {
             Result.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 排序原文
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Action action = new Action(() =>
+            {
+                Clear();
+                Result.AppendText(string.Format("开始装逼{0}", Environment.NewLine));
+                Initialize();
+                var files = Directory.GetFiles(Pathnew, "*.properties", SearchOption.TopDirectoryOnly).ToList();//目标目录
+                //var oldfiles = Directory.GetFiles(Pathold, "*.properties", SearchOption.TopDirectoryOnly).ToList();//目标目录
+                //for (int i = 0; i < files.Count; i++)
+                //for (int i = 0; i < 1; i++)
+                for (int i = 0; i < files.Count; i++)
+                {
+                    //遍历文件，然后从旧的文件找出可以汉化的部分，然后替换掉=号后面的内容
+                    string propertiePath = files.ElementAt(i);//propertie全路径
+                    string fileName = propertiePath.Split('\\').Last();
+                    //Result.AppendText(string.Format("开始分析文件：{1}{0}", Environment.NewLine, propertiePath));
+                    StringBuilder sb = new StringBuilder();//记录汉化文
+                    StringBuilder sb2 = new StringBuilder();//记录原文
+                    StringBuilder sb4 = new StringBuilder();//记录原文
+                    List<string> old = new List<string>();
+                    using (StreamReader sr = new StreamReader(propertiePath, Encoding.ASCII))
+                    {
+                        int line = 0;
+                        int duoyuLine = 0;
+                        while (!sr.EndOfStream)
+                        {
+                            string a = sr.ReadLine();//原文
+                            if (string.IsNullOrWhiteSpace(a) || !a.Contains("=") || a.StartsWith("#"))
+                            {
+                                sb4.AppendLine(a);
+                                line++;
+                                duoyuLine++;
+                                continue;
+                            }
+                            line++;
+                            old.Add(a);
+                            //Result.AppendText(string.Format("{2}行为：{1}{0}", Environment.NewLine, a, line++));
+                            using (StreamReader sr2 = new StreamReader(Path.Combine(Pathold, fileName), Encoding.ASCII))
+                            //读取已汉化文件
+                            {
+                                //打开新文件，然后遍历旧文件的每一行，然后替换，追加到sb,最后把sb输出到新目录里面
+                                List<string> oldTextList = new List<string>();
+                                while (!sr2.EndOfStream)
+                                {
+                                    oldTextList.Add(sr2.ReadLine());
+                                }
+                                //oldTextList = oldTextList.OrderBy(o => o.ToString()).ToList();
+                                string newhead = a.Split('=').First();//原文头部
+                                string fine = oldTextList.Where(o => o.StartsWith(newhead)).FirstOrDefault();//汉化文
+                                if (fine != null)//有汉化文
+                                {
+                                    sb.AppendLine(fine);//汉化文
+                                }
+                                else
+                                {
+                                    sb2.AppendLine(a);//原文
+                                }
+                            }
+                        }
+                        Result.AppendText(string.Format("原文：{0}行，多余行：{1}排序后：{2}行;出入行：{3}{4}", line, duoyuLine, old.Count, line - duoyuLine - old.Count, Environment.NewLine));
+                    }
+                    old = old.OrderBy(o => o.ToString()).ToList();
+                    StringBuilder sb3 = new StringBuilder();
+                    for (int j = 0; j < old.Count; j++)
+                    {
+                        sb3.AppendLine(old.ElementAt(j));
+                    }
+                    using (FileStream fs = new FileStream(Path.Combine(textBox5.Text, fileName), FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                    {//排序原文
+                        var bytes = System.Text.Encoding.ASCII.GetBytes(sb3.ToString());
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                    if (sb4.ToString().Length > 0)
+                    {
+                        using (FileStream fs = new FileStream(Path.Combine(@"F:\MyOpenSourceLife\resources_zh-cn\MyTextAnalyzer\duoyu", fileName), FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                        {//排序原文
+                            var bytes = System.Text.Encoding.ASCII.GetBytes(sb4.ToString());
+                            fs.Write(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+                Result.AppendText(string.Format("装逼结束{0}", Environment.NewLine));
+            });
+            TryCatch(action);
+            sw.Stop();
+            Result.AppendText(string.Format("耗时：{0}毫秒", sw.ElapsedMilliseconds));
+            //Result.AppendText(string.Format(""));
         }
     }
 }
